d3eae3c08cf4bf98a04cf625eab48bde85b949d5
AhmedWhoCode <atehseen044@gmail.com>
2024-06-20 16:25:34 +0100

    Initial Commit

---
+++ Gov.Services/Gov_ServicesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gov_ServicesApp.swift
+//  Gov.Services
+//
+//  Created by Tehseen Ahmed on 20/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Gov_ServicesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
